6d74d13b406742dce6664ca7aa7bcb299ea7f968
José Nery <josenerydev@gmail.com>
2020-06-19 18:05:27 -0300

    Complete sample app

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,19 @@ const DUMMY_PRODUCTS = [];
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
 app.get('/products', (req, res, next) => {
   res.status(200).json({ products: DUMMY_PRODUCTS })
 })
